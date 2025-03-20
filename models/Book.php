2a221ae84939610e1ec146b3fedf5aa82bfbb1a5
Kirill <mfriend@yandex.ru>
2025-03-20 21:02:02 +0300

    Init

--- models/Book.php
+++ models/Book.php
@@ -2,8 +2,20 @@
 
 namespace app\models;
 
+use queries\CatalogQuery;
+use yii\base\InvalidConfigException;
+use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
 
+/**
+ * @property string $title
+ * @property string $publication_year
+ * @property string $isbn
+ * @property ?string $main_page_image
+ * @property ?string $description
+ * @property ?string $deleted_at
+ * @property Author $authorList
+ */
 class Book extends ActiveRecord
 {
     public static function tableName(): string
@@ -14,7 +26,7 @@ class Book extends ActiveRecord
     public function rules(): array
     {
         return [
-            [['title', 'year', 'isbn'], 'required'],
+            [['title', 'publication_year', 'isbn'], 'required'],
             [['title', 'description', 'isbn', 'main_page_image'], 'string'],
             [['publication_year'], 'trim'],
             [
@@ -22,6 +34,25 @@ class Book extends ActiveRecord
                 'date',
                 'format' => 'php:Y-m-d',
             ],
+            [
+                'deleted_at',
+                'date',
+                'format' => 'php:Y-m-d H:i:s',
+            ],
         ];
+    }
+
+    public static function find(): CatalogQuery
+    {
+        return new CatalogQuery(static::class);
+    }
+
+    /**
+     * @throws InvalidConfigException
+     */
+    public function getAuthorList(): ActiveQuery
+    {
+        return $this->hasMany(Author::class, ['id' => 'author_id'])
+            ->viaTable(BookToAuthor::tableName(), ['book_id' => 'id']);
     }
 }
